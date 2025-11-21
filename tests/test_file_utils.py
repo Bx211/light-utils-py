acebbb17f783a360d55781d945c9a202582e502b
Bx211 <h123.bocah3@gmail.com>
2025-11-22 02:11:03 +0700

    Update test_file_utils.py

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,5 +1,5 @@
-from light_utils.time_utils import to_hms
+from light_utils.file_utils import read_json_safe
 
 
-def test_to_hms():
-assert to_hms(3600) == "01:00:00"
+def test_read_json_safe():
+assert read_json_safe("non_exist.json") is None
