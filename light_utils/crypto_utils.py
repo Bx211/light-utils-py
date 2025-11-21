a24759ce01644e44bd51b2c5ee64755ca0d7694a
Bx211 <h123.bocah3@gmail.com>
2025-11-22 02:08:01 +0700

    Update crypto_utils.py

--- light_utils/crypto_utils.py
+++ light_utils/crypto_utils.py
@@ -1,1 +1,13 @@
+import hashlib
 
+
+
+
+def sha256_hash(text: str) -> str:
+return hashlib.sha256(text.encode()).hexdigest()
+
+
+
+
+def md5_hash(text: str) -> str:
+return hashlib.md5(text.encode()).hexdigest()
